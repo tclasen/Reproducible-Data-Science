ab5e947cb79d62727479348a0ee6d151fb357ec6
Tory Clasen <ToryClasen@gmail.com>
2019-07-17 01:32:10 +0000

    Added standard jupyter server file

---
+++ 0_start_jupyter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source config.env
+
+docker run \
+    --rm \
+    -it \
+    -p 8080:8888 \
+    -v "$PWD":/home/jovyan/work \
+    --name jupyter \
+    jupyter/datascience-notebook:$JUPYTER_DOCKER_TAG \
+    start.sh jupyter lab --LabApp.token="$JUPYTER_PASSWORD"

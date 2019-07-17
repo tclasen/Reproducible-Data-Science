34a4b40f9a4cfcc1cd44931fbc8b9d10f9f2a70b
Tory Clasen <ToryClasen@gmail.com>
2019-07-17 01:36:51 +0000

    Added script to run fn2hash and fn2yara on all samples downloaded

---
+++ 2_hash_samples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source config.env
+
+if [ ! -d hashes ]; then
+    mkdir hashes
+
+    docker run \
+        --rm \
+        -it \
+        -v "$PWD"/samples:/archive \
+        -v "$PWD"/hashes:/hashes \
+        -w /hashes \
+        --name pharos \
+        eschwartz/pharos:$PHAROS_DOCKER_TAG \
+        /bin/bash -c "\
+            for i in /archive/*; do \
+                fn2hash \$i; \
+            done > hashes.csv;\
+            for i in /archive/*; do \
+	        fn2yara \$i; \
+	    done"
+fi
